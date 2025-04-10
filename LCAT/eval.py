929cc149be1a0f5ef47285ff2336e9a31b287f6f
KejiHan <36752344+KejiHan@users.noreply.github.com>
2025-04-10 21:25:45 +0800

    Update eval.py

--- LCAT/eval.py
+++ LCAT/eval.py
@@ -78,44 +78,6 @@ print('==> Building model..')
 
 
 
-
-class EMA(object):
-    def __init__(self, model, alpha=0.999, buffer_ema=True):
-        self.step = 0
-        self.model = copy.deepcopy(model)
-        self.alpha = alpha
-        self.buffer_ema = buffer_ema
-        self.shadow = self.get_model_state()
-        self.backup = {}
-        self.param_keys = [k for k, _ in self.model.named_parameters()]
-        self.buffer_keys = [k for k, _ in self.model.named_buffers()]
-
-    def update_params(self, model):
-        decay = min(self.alpha,(self.step + 1)/(self.step + 10))
-        state = model.state_dict()
-        for name in self.param_keys:
-            self.shadow[name].copy_(decay * self.shadow[name] + (1 - decay) * state[name])
-        for name in self.buffer_keys:
-            if self.buffer_ema:
-                self.shadow[name].copy_(decay * self.shadow[name] + (1 - decay) * state[name])
-            else:
-                self.shadow[name].copy_(state[name])
-        self.step += 1
-
-    def apply_shadow(self):
-        self.backup = self.get_model_state()
-        self.model.load_state_dict(self.shadow)
-
-    def restore(self):
-        self.model.load_state_dict(self.backup)
-
-    def get_model_state(self):
-        return {
-            k: v.clone().detach()
-            for k, v in self.model.state_dict().items()
-        }
-
-
 def eval_cifar():
     #attck_list = ['apgd-ce', 'apgd-dlr', 'fab', 'square', 'apgd-t', 'fab-t']
     attack_list=['apgd-ce','square']
@@ -134,8 +96,8 @@ def eval_cifar():
         max_values = out_.values
         out_= torch.relu((max_values[:, 0] - max_values[:,num_classes-1]))
         pred=outs.max(1)[1]
-        correct=pred.eq(targets).sum()
-        print(correct/len(inputs))
+        correct+=pred.eq(targets).sum()
+        
         tmp_len+=len(inputs)
 
         correct=correct.item()
